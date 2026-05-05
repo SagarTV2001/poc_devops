77df53987994e6038b642f0a0accd33eb970f5e7
SagarTV2001 <tvsagar404@gmail.com>
2026-05-05 17:15:42 +0530

    Update calculator options and add multiplication

--- main.c
+++ main.c
@@ -11,8 +11,10 @@ int main() {
     printf("Simple Calculator\n");
     printf("1. Add\n");
     printf("2. Subtract\n");
-    printf("3. Square Area\n");
-    printf("4. Rectangle Area\n");
+    printf("3. Multiply\n");
+    printf("4. Square Area\n");
+    printf("5. Rectangle Area\n");
+    
 
     printf("Enter your choice: ");
     scanf("%d", &choice);
@@ -31,15 +33,21 @@ int main() {
             result = sub(num1, num2);
             printf("Result = %d\n", result);
             break;
+                case 3:
+            printf("Enter two numbers: ");
+            scanf("%d %d", &num1, &num2);
+            result = mul(num1, num2);
+            printf("Result = %d\n", result);
+            break;
 
-        case 3:
+        case 4:
             printf("Enter side: ");
             scanf("%d", &num1);
             result = squareArea(num1);
             printf("Square Area = %d\n", result);
             break;
 
-        case 4:
+        case 5:
             printf("Enter length and width: ");
             scanf("%d %d", &num1, &num2);
             result = rectangleArea(num1, num2);
