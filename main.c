73dfe7c1914c3005a4e84afb84922c8cbbf39413
SagarTV2001 <tvsagar404@gmail.com>
2026-05-06 11:53:38 +0530

    Remove area calculation options from calculator
    Removed area calculation options from the menu.

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "calculator.h"
-#include "area.h"
 
 int main() {
     int num1;
@@ -12,8 +11,7 @@ int main() {
     printf("1. Add\n");
     printf("2. Subtract\n");
     printf("3. Multiply\n");
-    printf("4. Square Area\n");
-    printf("5. Rectangle Area\n");
+  
     
 
     printf("Enter your choice: ");
@@ -38,20 +36,6 @@ int main() {
             scanf("%d %d", &num1, &num2);
             result = mul(num1, num2);
             printf("Result = %d\n", result);
-            break;
-
-        case 4:
-            printf("Enter side: ");
-            scanf("%d", &num1);
-            result = squareArea(num1);
-            printf("Square Area = %d\n", result);
-            break;
-
-        case 5:
-            printf("Enter length and width: ");
-            scanf("%d %d", &num1, &num2);
-            result = rectangleArea(num1, num2);
-            printf("Rectangle Area = %d\n", result);
             break;
 
         default:
